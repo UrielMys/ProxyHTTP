672b08ddd9fb2bc8d8d0eeb10d1c63c7ac5d9f9e
Uriel <uri_mysler@hotmail.com>
2015-04-09 10:16:32 -0300

    Andando con los gets

--- hello.js
+++ hello.js
@@ -1,29 +1,46 @@
 var http = require('http');
-var https = require('https')
+var https = require('https');
+var url = require('url');
 
-var server=http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-  var data = '';
+var server=http.createServer(function (browserRequest, browserResponse) {
+  console.log(browserRequest.headers)
   var options = {
-  host: 'www.api.mercadolibre.com',
-  port: 80,
-  method: 'GET'
+  host: 'api.mercadolibre.com',
+  path: url.parse(browserRequest.url).path,
+  method: browserRequest.method
 };
-  options.path=request.url
-  console.log(options)
-  var req = https.get(options, function(res) {
-  console.log("volvio")
+  if(options.path=='/favicon.ico'){
+  	browserResponse.writeHead(200, {'Content-Type': 'text/plain'});
+  	browserResponse.end('Error');
+  	return
+  }
+  var apiRequest = https.request(options, function(apiResponse) { //el request http
+  
+  apiResponse.on('error',function(e){
+   console.log( e.stack );
+  });
+  var textoApi=""; //el que va al browser
+  apiResponse.on('data',function(datos){ //cosas de la api al browser
+  	textoApi+=datos;
+  });
+  apiResponse.on('end',function(){ //cierro la conxion con la api
+  	browserResponse.writeHead(200, {'Content-Type': 'application/json'}); 
+  	browserResponse.end(textoApi);//cierro la conexion con el browser
+  });
 });
-  console.log(request.url)
-  console.log(request.connection.remoteAddress) //ip que origina la conexion
-
+  var textoBrowser=""; //el que va a la api
+  browserRequest.on('data',function(datos){
+  	textoBrowser+=datos
+  })
+  browserRequest.on('end',function(){
+  	apiRequest.write(textoBrowser);
+  	apiRequest.end();	
+  })
+  //console.log(request.url)
+  //console.log(request.connection.remoteAddress) //ip que origina la conexion
+  
 });
 server.listen(8124);
 
 
 console.log('Server running at http://127.0.0.1:8124/');
-
-exports.world = function() {
-  console.log('Hello World');
-}
