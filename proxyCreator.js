f87a4d29915737bcdbd752d4698eb27e3d958784
Uriel <uri_mysler@hotmail.com>
2015-04-10 10:32:12 -0300

    conexion con la base de redis

--- proxyCreator.js
+++ proxyCreator.js
@@ -2,35 +2,64 @@ var http = require('http');
 var https = require('https');
 var url = require('url');
 var serverPort;
+var redis = require('redis');
+var client = redis.createClient();
+client.on('connect',function(){
+	console.log('se conecto a la base de datos')
+});
+client.on('error',function(){
+	console.log('hubo un error con la base de datos')
+});
 var createProxy=function(configurations){
-var server=http.createServer(function (browserRequest, browserResponse) {
-var theHeaders=browserRequest.headers;
-	delete theHeaders["host"];
-	serverPort=configurations ? configurations.serverPort : 8124
-  var options = {
-  headers:theHeaders,
-  host: configurations ? configurations.host : 'api.mercadolibre.com',
+
+	var server=http.createServer(function (browserRequest, browserResponse) {
+		var theHeaders=browserRequest.headers;
+		delete theHeaders["host"];
+		serverPort=configurations ? configurations.serverPort : 8124
+		var options = {
+			headers:theHeaders,
+			host: configurations ? configurations.host : 'api.mercadolibre.com',
   port: configurations ? configurations.destinationHost : 443, //443 es el default de https
   path: url.parse(browserRequest.url).path,
   method: browserRequest.method,
 };
-  if(options.path=='/favicon.ico'){
-  	browserResponse.writeHead(200, {'Content-Type': 'text/plain'});
-  	browserResponse.end('Error');
-  	return
-  }
+
+		if(options.path=='/favicon.ico'){
+			browserResponse.writeHead(200, {'Content-Type': 'text/plain'});
+			browserResponse.end('Error');
+			return
+		}		checkPossibleConnection(browserRequest,browserResponse,options);
+
+});
+server.listen(serverPort || 8124);
+function checkPossibleConnection(request,response,options){
+	client.sismember('ipFilter',request.connection.remoteAddress,function(err,reply){
+		console.log(reply);
+		if(reply==1){
+			sendForbidden(response);
+		}else{
+			makeRequest(request,response,options);
+			
+		}
+	});
+}
+function makeRequest(browserRequest,browserResponse,options){
+
   var apiRequest = https.request(options, function(apiResponse) { //el request http
   apiResponse.pipe(browserResponse); //fuerzo a que todo lo que se recibe de la api vaya al browser
   	browserResponse.writeHead(apiResponse.statusCode,apiResponse.headers); //se asegura que le lleguen los headers al browser
-  
+
   //console.log(request.url)
   //console.log(request.connection.remoteAddress) //ip que origina la conexion
-  });
-  browserRequest.pipe(apiRequest); //manda los elementos de un put/post del browser a la api
-  apiRequest.end();
 });
-server.listen(serverPort || 8124);
-
+  browserRequest.pipe(apiRequest); //manda los elementos de un put/post del browser a la api
+  apiRequest.end();	
+}
+function sendForbidden(response){ //no le permite el acceso al usuario
+	console.log('forbidden');
+	response.writeHead(403,{'Content-Type':'text/plain'});
+	response.end('forbidden');
+}
 
 console.log('Server running at http://127.0.0.1:8124/');
 };
